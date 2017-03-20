cd73e4428270a39d638ae61aaa02f65a4c31b383
sergey <krasnosergey@gmail.com>
2017-03-20 14:50:58 +0100

    Handle child VCs on unwind

--- TodoSimple/ViewController.swift
+++ TodoSimple/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     typealias PrepareBlock = (UIStoryboardSegue) -> Void
-
+    private var isChild = false
     private var seguePrepareblocks = [String: PrepareBlock]()
     func performSegue(withIdentifier identifier: String, sender: Any?, setupBlock: @escaping PrepareBlock) {
         self.seguePrepareblocks[identifier] = setupBlock
         self.performSegue(withIdentifier: identifier, sender: sender)
+    }
+
+    override func addChildViewController(_ childController: UIViewController) {
+        super.addChildViewController(childController)
+        if let viewController = childController as? ViewController {
+            viewController.isChild = true
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -25,7 +32,7 @@ class ViewController: UIViewController {
     
     override func canPerformUnwindSegueAction(_ action: Selector, from fromViewController: UIViewController,
                                               withSender sender: Any) -> Bool {
-        if fromViewController === self {
+        if fromViewController === self || self.isChild {
             return false // Just to be sure that we will not have segue with equal source and destination VC.
         }
         return true
